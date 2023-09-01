8b4541f0f4ee017072c578b11788c6a3b4f56287
cmlc717 <carinamariecarlos@gmail.com>
2023-09-01 16:26:51 -0400

    display single post

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import sqlite3
 
 from flask import Flask, render_template
 
+from werkzeug.exceptions import abort
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -15,3 +17,17 @@ def index():
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
